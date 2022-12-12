c225a4376087b08d2107ea6b55686800291a1ef5
Anju M <Anju@NTP-LAP-771.chn.nestgroup.net>
2022-12-12 14:22:19 +0530

    delete emp

--- src/main/java/com/example/NestDigitalApp_BackEnd/dao/EmployDao.java
+++ src/main/java/com/example/NestDigitalApp_BackEnd/dao/EmployDao.java
@@ -1,6 +1,8 @@
 package com.example.NestDigitalApp_BackEnd.dao;
 
 import com.example.NestDigitalApp_BackEnd.model.Employ;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -11,4 +13,8 @@ public interface EmployDao extends CrudRepository<Employ, Integer> {
 
     @Query(value = "SELECT `id`, `designation`, `empcode`, `empname`, `password`, `salary`, `username` FROM `empdetails` WHERE `empcode`= :empcode",nativeQuery = true)
     List<Employ> EmpSearch(@Param("empcode") Integer empcode);
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM `empdetails` WHERE `id`= :id",nativeQuery = true)
+    void DeleteEmploy(@Param("id") Integer id);
 }
