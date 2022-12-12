a9f284402126c20231391f20da5f33fecf7e7197
Anju M <Anju@NTP-LAP-771.chn.nestgroup.net>
2022-12-12 15:41:38 +0530

    security login

--- src/main/java/com/example/NestDigitalApp_BackEnd/dao/SecurityDao.java
+++ src/main/java/com/example/NestDigitalApp_BackEnd/dao/SecurityDao.java
@@ -1,7 +1,15 @@
 package com.example.NestDigitalApp_BackEnd.dao;
 
 import com.example.NestDigitalApp_BackEnd.model.Security;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface SecurityDao extends CrudRepository<Security,Integer> {
+
+
+    @Query(value = "SELECT `id`, `empcode`, `name`, `password`, `username` FROM `securitydetail` WHERE `username`= :username AND `password`= :password",nativeQuery = true)
+    List<Security> SecurityLogin(@Param("username") String username,@Param("password") String password);
 }
