954f459ef9dc620b1acb6ce803b0e9ccd5d87315
Anju M <Anju@NTP-LAP-771.chn.nestgroup.net>
2022-12-17 11:44:21 +0530

    counter

--- src/main/java/com/example/NestDigitalApp_BackEnd/dao/LeaveCounterDao.java
+++ src/main/java/com/example/NestDigitalApp_BackEnd/dao/LeaveCounterDao.java
@@ -7,7 +7,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 public interface LeaveCounterDao extends CrudRepository<LeaveCounter ,Integer> {
+
+
+    @Query(value = "SELECT `id`, `casual`, `empid`, `other`, `sick`, `year` FROM `leavecount` WHERE `empid`= :empid",nativeQuery = true)
+    List<LeaveCounter> Leaves(@Param("empid") Integer empid);
+
     @Modifying
     @Transactional
     @Query(value = "UPDATE `leavecount` SET `casual`= :casual,`other`= :other,`sick`= :sick WHERE `empid`= :empid",nativeQuery = true)
